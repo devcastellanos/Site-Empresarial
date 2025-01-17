86c313ff08db37f95686780f920b5ef99ed4a4ae
FelipeAvila <becario2.sis@grupotarahumara.com.mx>
2025-01-17 14:13:46 -0600

    add post chafa

--- src/app/posts.tsx
+++ src/app/posts.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Button,
   Typography,
@@ -11,77 +11,104 @@ import {
 import { ArrowSmallDownIcon } from "@heroicons/react/24/solid";
 import BlogPostCard from "@/components/blog-post-card";
 import Image from "next/image";
+import { Input } from "@material-tailwind/react";
+import path from "path";
+import axios from "axios";
 
-const POSTS = [
-  {
-    img: `/image/blogs/blog2.svg`,
-    tag: "Enterprise",
-    title: "The key new features and changes in Tailwind CSS",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "10 September 2022",
-    author: {
-      img: `/image/avatar1.jpg`,
-      name: "Ryan Samuel",
-    },
-  },
-  {
-    img: `/image/blogs/blog6.svg`,
-    tag: "Startups",
-    title: "Lyft launching cross-platform service this week",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "12 September 2022",
-    author: {
-      img: `/image/blogs/blog2.svg`,
-      name: "Nora Hazel",
-    },
-  },
-  {
-    img: `/image/blogs/blog3.svg`,
-    tag: "Trending",
-    title: "6 insights into the French Fashion landscape",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "16 September 2022",
-    author: {
-      img: `/image/avatar2.jpg`,
-      name: "Otto Gonzalez",
-    },
-  },
-  {
-    img: `/image/blogs/blog4.svg`,
-    tag: "Lifestyle",
-    title: "Autodesk looks to future of 3D printing with Project",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "18 September 2022",
-    author: {
-      img: `/image/avatar3.jpg`,
-      name: "Ryan Samuel",
-    },
-  },
-  {
-    img: `/image/blogs/blog5.svg`,
-    tag: "Enterprise",
-    title: "Autodesk looks to future of 3D printing with Project",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "10 September 2022",
-    author: {
-      img: `/image/avatar3.jpg`,
-      name: "Ryan Samuel",
-    },
-  },
-  {
-    img: `/image/blogs/blog6.svg`,
-    tag: "Startups",
-    title: "Lyft launching cross-platform service this week",
-    desc: "Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens bed design but the back is too high for the beams and angle of the ceiling I also wanted to point out.",
-    date: "12 September 2022",
-    author: {
-      img: `/image/avatar2.jpg`,
-      name: "Nora Hazel",
-    },
-  },
-];
+
+interface Post {
+  img: string;
+  tag: string;
+  title: string;
+  desc: string;
+  date: string;
+  img_author: string;
+  name_author: string;
+  num_empleado: number;
+  imgFile?: File;
+}
 
 export function Posts() {
+  const [post, setPost] = React.useState<Post | null>(
+    {
+      img: "",
+      tag: "",
+      title: "",
+      desc: "",
+      date: "",
+      img_author: "",
+      name_author: "",
+      num_empleado: 0,
+      imgFile: undefined,
+    }
+  );
+  const [posts, setPosts] = React.useState<Post[]>([]);
+  const [imgFile, setImgFile] = React.useState<File | null>(null);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+
+      const dataBlog = await fetch('http://localhost:3001/posts');
+      const posts = await dataBlog.json();
+      console.log(posts);
+      setPosts(posts);
+    }
+    fetchPosts();
+
+  }, []);
+
+  const handleAddPost = async () => {
+
+    if (!post?.imgFile || !post?.num_empleado) {
+      alert("Por favor, sube una imagen y proporciona el número de empleado.");
+      return;
+    }
+
+    const formData = new FormData();
+    if (post && post.imgFile instanceof File) { formData.append("image", post.imgFile); }
+    formData.append("num_empleado", post.num_empleado.toString());
+    formData.append("name_author", post.name_author);
+    formData.append("title", post.title);
+    formData.append("desc", post.desc);
+    formData.append("tag", post.tag);
+    formData.append("date", post.date);
+
+    try {
+      console.log("Guardando imagen...");
+      const res = await axios.post("/api/uploadImages", formData,  
+       {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log("Imagen guardada correctamente");
+
+      const newPost = {
+        ...post,
+        img:res.data.imageUrl,
+      };
+      
+      const response = await fetch('http://localhost:3001/Agregarpost', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPost),
+      });
+
+      if (!response.ok) {
+        throw new Error('Error en la solicitud');
+      }
+
+      const result = await response.json();
+      console.log('Post agregado con éxito', result);
+    } catch (error) {
+      console.error('Error al agregar post:', error);
+    }
+  };
+
   return (
     <section className="grid min-h-screen place-items-center p-8">
       <Tabs value="trends" className="mx-auto max-w-7xl w-full mb-16 ">
@@ -104,6 +131,99 @@ export function Posts() {
         layout="responsive"
         className="object-cover"
       />
+      <div>
+        <Typography variant="h1" className="mb-2 " placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+          Nuevo post
+        </Typography>
+        <Input
+          placeholder="Titulo"
+          crossOrigin=""
+          value={post?.title}
+          onChange={(e) => setPost({ ...post!, title: e.target.value })}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        />
+        
+        <Input
+          placeholder="Descripcion"
+          crossOrigin=""
+          value={post?.desc}
+          onChange={(e) => setPost({ ...post!, desc: e.target.value })}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        />
+        <Input
+          placeholder="Tag"
+          crossOrigin=""
+          value={post?.tag}
+          onChange={(e) => setPost({ ...post!, tag: e.target.value })}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        />
+        <Input
+          placeholder="Fecha"
+          crossOrigin=""
+          value={post?.date}
+          onChange={(e) => setPost({ ...post!, date: e.target.value })}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        />
+        <Input
+          placeholder="Imagen"
+          crossOrigin=""
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            if (e.target.files && e.target.files[0]) {
+              const file = e.target.files[0];
+              setImgFile(file);
+              if (post) {
+                setPost({ ...post, imgFile: file });
+              }
+            }
+          }}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        />
+        <Input
+          placeholder="Nombre Autor"
+          crossOrigin=""
+          value={post?.name_author}
+          onChange={(e) => setPost({ ...post!, name_author: e.target.value })}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        />
+        <Input
+          placeholder="Numero Empleado"
+          crossOrigin=""
+          value={post?.num_empleado}
+          onChange={(e) => setPost({ ...post!, num_empleado: Number(e.target.value) })}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        />
+        {post?.imgFile && (
+          <Image
+            src={URL.createObjectURL(post.imgFile)}
+            alt="Post image"
+            width={200}
+            height={200}
+            className="object-cover"
+          />
+        )}
+        
+        <Button
+          variant="filled"
+          color="blue"
+          className="flex items-center gap-2 mt-4"
+          placeholder=""
+          onClick={handleAddPost}
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        >
+          Agregar
+        </Button>
+
+      </div>
       <div className="py-8"></div>
       <Typography variant="h1" className="mb-2 " placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
         Trends News
@@ -121,7 +241,7 @@ export function Posts() {
         news.
       </Typography>
       <div className="container my-auto grid grid-cols-1 gap-x-8 gap-y-16 items-start lg:grid-cols-3">
-        {POSTS.map(({ img, tag, title, desc, date, author }) => (
+        {posts?.map(({ img, tag, title, desc, date, img_author, name_author  }) => (
           <BlogPostCard
             key={title}
             img={img}
@@ -129,10 +249,7 @@ export function Posts() {
             title={title}
             desc={desc}
             date={date}
-            author={{
-              img: author.img,
-              name: author.name,
-            }}
+            author={{ img: img_author, name: name_author }}
           />
         ))}
       </div>
