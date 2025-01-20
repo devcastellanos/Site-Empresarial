c146cac1a6f3f29dd5f83a7f9af29fefa36533c6
FelipeAvila <becario2.sis@grupotarahumara.com.mx>
2025-01-20 16:08:05 -0600

    Fotos y blog

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,10 @@
+import { Login } from '../signup';
+import { Navbar, Footer, BlogPostCard } from "@/components";
+
+export default function Campaign() {
+    return (
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <Login />
+      </div>
+    );
+  }
--- src/components/blog-post-card.tsx
+++ src/components/blog-post-card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import {
   Typography,
@@ -6,7 +6,15 @@ import {
   CardHeader,
   CardBody,
   Avatar,
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Input,
 } from "@material-tailwind/react";
+
+import { Post } from "../app/posts";
 
 
 interface BlogPostCardProps {
@@ -16,6 +24,9 @@ interface BlogPostCardProps {
   desc: string;
   author: { name: string; img: string };
   date: string;
+  idBlog: number;
+  num_empleado: number;
+  onPostEdit: (post: Post) => void;
 }
 
 export function BlogPostCard({
@@ -25,8 +36,51 @@ export function BlogPostCard({
   desc,
   author,
   date,
-}: BlogPostCardProps) {
+  idBlog,
+  num_empleado,
+  onPostEdit,}: BlogPostCardProps) {
+  const formattedUserId = num_empleado.toString().padStart(4, '0');
+  const [openModal, setOpenModal] = React.useState(false);
+  const [post, setPost] = React.useState<Post >(
+     {
+       idBlog: idBlog,
+       img: img,
+       tag: tag,
+       title: title,
+       desc: desc,
+       date: date,
+       img_author: author.img,
+       name_author: author.name,
+       num_empleado: num_empleado,
+     }
+   );
+
+  const handleEditClick = () => {
+    setOpenModal(true); // Abre el modal cuando se hace clic en "Editar"
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false); // Cierra el modal
+  };
+
+  const handleEdit = () => {
+    fetch(`http://localhost:3001/ActualizarPost`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setOpenModal(false);
+        onPostEdit(post);
+      });
+  }
+
   return (
+    <>
     <Card shadow={true} placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
       <CardHeader placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
         <Image
@@ -72,7 +126,7 @@ export function BlogPostCard({
           <Avatar
             size="sm"
             variant="circular"
-            src={author.img}
+            src={`/fotos/${formattedUserId}.jpg`}
             alt={author.name}
             placeholder=""
             onPointerEnterCapture={() => {}}
@@ -99,10 +153,96 @@ export function BlogPostCard({
             >
               {date}
             </Typography>
+           
           </div>
+          <Button 
+          className="ml-auto"
+          placeholder="" 
+          onPointerEnterCapture={() => {}} 
+          onPointerLeaveCapture={() => {}}
+          onClick={handleEditClick}>
+            
+              Editar
+          </Button>
         </div>
       </CardBody>
     </Card>
+
+    {/* Modal para editar información */}
+    <Dialog 
+      open={openModal} 
+      handler={handleCloseModal} 
+      placeholder="" 
+      onPointerEnterCapture={() => {}} 
+      onPointerLeaveCapture={() => {}}>
+    <DialogHeader 
+      placeholder="" 
+      onPointerEnterCapture={() => {}} 
+      onPointerLeaveCapture={() => {}}>
+      Edita la información
+    </DialogHeader>
+    <DialogBody 
+      placeholder="" 
+      onPointerEnterCapture={() => {}} 
+      onPointerLeaveCapture={() => {}}>
+      <div>
+        {/* Aquí puedes agregar los campos del formulario de edición */}
+        <Input
+          type="text"
+          placeholder="Título"
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+          className="w-full p-2 border rounded mb-4"
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+          crossOrigin=""
+        />
+        <Input
+          placeholder="Descripción"
+          value={post.desc}
+          onChange={(e) => setPost({ ...post, desc: e.target.value })}
+          className="w-full p-2 border rounded mb-4"
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+          crossOrigin=""
+        />
+        <Input
+          type="text"
+          placeholder="Tag"
+          value={post.tag}
+          onChange={(e) => setPost({ ...post, tag: e.target.value })}
+          className="w-full p-2 border rounded mb-4"
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+          crossOrigin=""
+        />
+
+      </div>
+    </DialogBody>
+    <DialogFooter 
+      placeholder="" 
+      onPointerEnterCapture={() => {}} 
+      onPointerLeaveCapture={() => {}}>
+      <Button 
+        variant="text" 
+        color="red" 
+        onClick={() => {  handleCloseModal(); }}
+        placeholder="" 
+        onPointerEnterCapture={() => {}} 
+        onPointerLeaveCapture={() => {}}>
+        Cerrar
+      </Button>
+      <Button 
+        variant="gradient" 
+        onClick={() =>{handleEdit();handleCloseModal();} }
+        placeholder="" 
+        onPointerEnterCapture={() => {}} 
+        onPointerLeaveCapture={() => {}}>
+        Guardar
+      </Button>
+    </DialogFooter>
+  </Dialog>
+    </>
   );
 }
 
