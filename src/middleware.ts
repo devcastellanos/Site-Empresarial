daf0bd33e7854ec0015ca9f2fcda7db40d42371a
LuisAvila08 <becario2.sis@grupotarahumara.com.mx>
2025-04-30 13:48:28 -0600

    new routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,5 +34,5 @@ export async function middleware(request: NextRequest) {
 // };
 
 export const config = {
-  matcher: [], // Aplica el middleware solo a estas rutas
+  matcher: ['/Cursos', '/cargaMasiva', '/Usuarios', '/Perfil'], // Aplica el middleware solo a estas rutas
 };
