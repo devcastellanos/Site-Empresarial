53ad24938b7a23d63aacecbfc46dfc69b5947abb
FelipeAvila <becario2.sis@grupotarahumara.com.mx>
2025-06-04 09:14:25 -0600

    suspense in convenios

--- src/app/Convenios/page.tsx
+++ src/app/Convenios/page.tsx
@@ -1,5 +1,6 @@
 // components
 import { NavbarRH, FooterRH, BlogPostCard } from "@/components";
+import { Suspense } from "react";
 
 // sections
 
@@ -8,13 +9,13 @@ import Convenio from "@/components/agreementsComponents/convenio";
 
 
 export default function Campaign() {
-    return (
-      <>
-        <NavbarRH />
-  
+  return (
+    <>
+      <NavbarRH />
+
+      <Suspense fallback={<div className="text-center mt-10">Cargando convenios...</div>}>
         <Convenio />
-      </>
-  
-    );
-  }
-  
+      </Suspense>
+    </>
+  );
+}
